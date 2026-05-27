deacd6d1e2975d66112d180624244da1c45c9cfa
Thien Huynh <hmthienajcr@gmail.com>
2026-05-27 21:48:47 +0700

    chore: release v2.0.0

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -38,7 +38,7 @@ async function runTypeScriptCli() {
   program
     .name('loci')
     .description('Local ticket management tool')
-    .version('1.0.2')
+    .version('2.0.0')
 
   program.addCommand(initCommand)
   program.addCommand(addCommand)
